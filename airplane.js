976cb1aa49decc32bc724d47780e0fa6500685aa
ashnel <ashleyjnelms@gmail.com>
2017-12-27 14:40:09 -0800

    added enemy movement and missile shooting

--- airplane.js
+++ airplane.js
@@ -30,6 +30,8 @@ var enemies = [
     }
 ];
 
+var missiles = [];
+
 function drawPlayer () {
     var content = "<div class='player' style='left:" + player.left + "px; top:" + player.top + "px'></div>";
     
@@ -42,6 +44,27 @@ function drawEnemies () {
         content += "<div class='enemy' style='left:" + enemies[i].left + "px; top:" + enemies[i].top + "px'></div>";
     }
     document.getElementById("enemies").innerHTML = content;
+}
+
+function drawMissiles () {
+    var content = "";
+    for(var i = 0; i < missiles.length; i++) {
+        content += "<div class='missile' style='left:" + missiles[i].left + "px; top:" + missiles[i].top + "px'></div>";
+    }
+    document.getElementById("missiles").innerHTML = content;
+    console.log("content:" + content);
+}
+
+function moveEnemies () {
+    for (var i = 0; i < enemies.length; i++) {
+        enemies[i].top = enemies[i].top + 6;
+    }
+}
+
+function moveMissiles () {
+    for (var i = 0; i < missiles.length; i++) {
+        missiles[i].top = missiles[i].top - 20;
+    }
 }
 
 document.onkeydown = function (e) {
@@ -57,8 +80,25 @@ document.onkeydown = function (e) {
     if (e.keyCode == 40) { // Down
         player.top = player.top + 10;
     }
+    if (e.keyCode == 32) { // Shoot
+        missiles.push({left: (player.left + 20), top: (player.top - 5)})
+        drawMissiles();
+    }
     drawPlayer();
 }
 
-drawPlayer();
-drawEnemies();
+function gameLoop () {
+    console.log("GameLoop is running");
+    
+    drawPlayer();
+    
+    moveEnemies();
+    drawEnemies();
+    
+    moveMissiles();
+    drawMissiles();
+    
+    setTimeout(gameLoop, 100);
+}
+
+gameLoop();
